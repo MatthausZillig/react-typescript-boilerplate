d192f9b5901d20b06576d890cfd1390199e632cc
Matthaus <matthauszigillig@hotmail.com>
2020-08-24 20:13:36 -0300

    chore: add typescript / ts-node-dev / express / @types/espress

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+﻿import express from 'express';
+
+const app = express();
+
+app.get('/', (required, response) => {
+	return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => {
+	console.log('🦄 Server started on port 3333!');
+});
